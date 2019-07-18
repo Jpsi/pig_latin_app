2c798753f0eee7f578f15cfe60f9b912851ede1c
Philip Fol <philip.fol08@gmail.com>
2019-07-18 21:07:05 +0100

    Use function instead of class for React component

--- app/javascript/components/ReactApp.js
+++ app/javascript/components/ReactApp.js
@@ -2,17 +2,15 @@ import React from 'react';
 import logo from 'images/pig.png';
 import Form from 'components/Form.js';
 
-class ReactApp extends React.Component {
-  render() {
-    return (
-      <div className="ReactApp">
-        <header className="ReactApp-header">
-          <img src={logo} className="ReactApp-logo" alt="logo" />
-        </header>
-        <Form />
-      </div>
-    );
-  }
+function ReactApp() {
+  return (
+    <div className="ReactApp">
+      <header className="ReactApp-header">
+        <img src={logo} className="ReactApp-logo" alt="logo" />
+      </header>
+      <Form />
+    </div>
+  );
 }
 
 export default ReactApp;
